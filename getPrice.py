42e2df386e82f0d3ae4321720f27aea2057cdbbc
CodeWithJoe2020 <75099121+CodeWithJoe2020@users.noreply.github.com>
2023-04-15 13:35:17 +0200

    Create getPrice.py

---
+++ getPrice.py
@@ -0,0 +1,26 @@
+from web3 import Web3 
+from abis import TOKENLPABI, ABI
+import decimal
+from getEthPrice import ethPrice
+import config
+
+node = config.node
+web3 = Web3(Web3.HTTPProvider(node))
+
+
+
+lp_address = web3.to_checksum_address(config.lpAddress)
+lpContract = web3.eth.contract(address=lp_address, abi=TOKENLPABI)
+tokenAddress = web3.to_checksum_address(config.tokenAddress)
+waterfallContract = web3.eth.contract(address=tokenAddress, abi=ABI)
+
+def token_price():
+    reserves = lpContract.functions.getReserves().call()
+    price = reserves[0] / reserves[1]
+    priced = decimal.Decimal(price).quantize(decimal.Decimal('0.00000001'))
+    return priced
+
+def token_price_usd():
+    tokenPriceEth = token_price()
+    tokenPriceUsd = float(tokenPriceEth) * float(ethPrice())
+    return tokenPriceUsd
