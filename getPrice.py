8db202b633c14e32788c5924923f79fcaf61e202
CodeWithJoe2020 <75099121+CodeWithJoe2020@users.noreply.github.com>
2023-04-16 12:26:51 +0200

    Update getPrice.py

--- getPrice.py
+++ getPrice.py
@@ -1,26 +1,53 @@
+#https://eth-mainnet.g.alchemy.com/v2/-Oo6Y1F3-1XO8vvIg6inpWboi0itglZF
+
 from web3 import Web3 
-from abis import TOKENLPABI, ABI
+from abi import ERC20ABI, LPABI
 import decimal
-from getEthPrice import ethPrice
+#from getEthPrice import getEthPrice
 
-
-node = config.node
+node = 'get 100$ free credit using this link here https://bit.ly/3x67pnF alchemy node'
 web3 = Web3(Web3.HTTPProvider(node))
 
+#print(web3.is_connected())
+
+#tokenAddress = web3.to_checksum_address('0x1f9840a85d5af5bf1d1762f925bdaddc4201f984')
+#tokenContract = web3.eth.contract(address=tokenAddress, abi=ERC20ABI)
+
+lpAddress=web3.to_checksum_address('0xA43fe16908251ee70EF74718545e4FE6C5cCEc9f')
+lpContract = web3.eth.contract(address=lpAddress, abi=LPABI)
 
 
-lp_address = web3.to_checksum_address(config.lpAddress)
-lpContract = web3.eth.contract(address=lp_address, abi=TOKENLPABI)
-tokenAddress = web3.to_checksum_address(config.tokenAddress)
-waterfallContract = web3.eth.contract(address=tokenAddress, abi=ABI)
 
 def token_price():
-    reserves = lpContract.functions.getReserves().call()
-    price = reserves[0] / reserves[1]
-    priced = decimal.Decimal(price).quantize(decimal.Decimal('0.00000001'))
-    return priced
+    # Get the addresses of the two tokens in the pair
+    token0, token1 = lpContract.functions.token0().call(), lpContract.functions.token1().call()
+    print("Token0:", token0)
+    print("Token1:", token1)
 
-def token_price_usd():
-    tokenPriceEth = token_price()
-    tokenPriceUsd = float(tokenPriceEth) * float(ethPrice())
-    return tokenPriceUsd
+    # Get the decimals of the two tokens
+    token0c = web3.eth.contract(address=token0, abi=ERC20ABI)
+    token1c = web3.eth.contract(address=token1, abi=ERC20ABI)
+    decimals0 = token0c.functions.decimals().call()
+    decimals1 = token1c.functions.decimals().call()
+
+    # Get the current reserves of the two tokens
+    reserves = lpContract.functions.getReserves().call()
+    reserve0 = float(reserves[0]) / 10 ** decimals0
+    reserve1 = float(reserves[1]) / 10 ** decimals1
+    print("Reserve0:", reserve0)
+    print("Reserve1:", reserve1)
+
+    # Calculate the token price
+    price = reserve1 / reserve0
+
+    # Format the token price as a string with 8 decimal places
+    price_formatted = "{:.15f} {} per {}".format(price, token1c.functions.symbol().call(), token0c.functions.symbol().call())
+    
+    return price_formatted
+    
+def usd_price():
+    usdPrice= float(token_price()) * float(getEthPrice())
+    return usdPrice
+
+print(token_price())
+print(usd_price())
